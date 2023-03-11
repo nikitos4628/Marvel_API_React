a851af5b7357747b1071379295933e40f25952bb
nikitos4628 <soba.nikita@mail.ru>
2023-03-11 07:48:20 +0700

    ref, focus, tabindex

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -59,8 +59,20 @@ class CharList extends Component {
 		})
 	}
 
+	itemRefs = [];
+
+	setRef = (ref) => {
+		this.itemRefs.push(ref)
+	}
+
+	focusOnItem = (id) => {
+		this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+		this.itemRefs[id].classList.add('char__item_selected');
+		this.itemRefs[id].focus();
+	}
+
 	renderItems(arr) {
-		const items = arr.map((item) => {
+		const items = arr.map((item, i) => {
 			let imgStyle = {'objectFit' : 'cover'};
 			if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
 				imgStyle = {'objectFit' : 'unset'};
@@ -69,8 +81,20 @@ class CharList extends Component {
 			return (
 				<li 
 					className="char__item"
+					tabIndex={0}
+					ref={this.setRef}
 					key={item.id}
-					onClick={() => this.props.onCharSelected(item.id)}>
+					onClick={() => {
+						this.props.onCharSelected(item.id);
+						this.focusOnItem(i);
+						}}
+					onKeyPress={(e) => {
+						if (e.key === ' ' || e.key === 'Enter') {
+							this.props.onCharSelected(item.id);
+							this.focusOnItem(i);
+						}
+					}}>
+
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
             </li>
@@ -104,7 +128,7 @@ class CharList extends Component {
 				style={{'display': charEnded ? 'none' : 'block'}}
 				disabled={newItemLoading}
 				onClick={() => this.onRequest(offset)}>
-                <div className="inner">load more</div>
+            <div className="inner">load more</div>
             </button>
 			</div>
 		)
