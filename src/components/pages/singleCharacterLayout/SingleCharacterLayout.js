9b5975fd35cf1df4d00c3a380abf5351febe65db
nikitos4628 <soba.nikita@mail.ru>
2023-03-23 18:39:39 +0700

    react-transition-group, react-helmet, SCL back btn

--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import Helmet from "react-helmet";
+
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({data}) => {
@@ -6,11 +9,19 @@ const SingleCharacterLayout = ({data}) => {
 
 	return (
 		<div className="single-comic">
-				<img src={thumbnail} alt={name} className="single-comic__char-img"/>
-				<div className="single-comic__info">
-					<h2 className="single-comic__name">{name}</h2>
-					<p className="single-comic__descr">{description}</p>
-				</div>
+			<Helmet>
+				<meta
+					name="description"
+					content={`${name} character`}
+				/>
+				<title>{name}</title>
+			</Helmet>
+			<img src={thumbnail} alt={name} className="single-comic__char-img"/>
+			<div className="single-comic__info">
+				<h2 className="single-comic__name">{name}</h2>
+				<p className="single-comic__descr">{description}</p>
+			</div>
+			<Link to="/" className="single-comic__back">Back to all</Link>
 		</div>
 	)
 }
